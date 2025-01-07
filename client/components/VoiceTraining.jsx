6b00c06a80fa5b3fc2285be7883e6eb6398cb8d3
coltongraygg <coltongraygg@gmail.com>
2025-01-07 14:06:08 -0600

    INIT component for voice training feat

--- client/components/VoiceTraining.jsx
+++ client/components/VoiceTraining.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-export default function voiceTraining() {
+export default function VoiceTraining() {
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
-        <div>
-            <h1>Hello!! Look at this pop up!</h1>
-        </div>
+        <>
+        <Button variant="primary" onClick={handleShow}>
+            Launch modal
+        </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Header Here</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, we got text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close Modal
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save changes or do something else
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     )
 }
 
