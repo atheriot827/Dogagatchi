a829c9a4da86df486e0db2f1e4cf295fc267f8a0
coltongraygg <coltongraygg@gmail.com>
2025-01-07 12:18:32 -0600

    ADD .idea/ config to .gitignore

--- server/db/index.js
+++ server/db/index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ATLAS_URI } = require('../config');
 
 mongoose
-  .connect(ATLAS_URI)
+  .connect(ATLAS_URI) // Changed this to fix mongodb atlas connection
   .then(() => console.log('Database connected!'))
   .catch((err) => console.log(err));
 
