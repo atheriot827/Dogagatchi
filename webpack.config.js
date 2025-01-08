17f6ce93b5d8d0aa170c36bf9aaaf9125e10e911
bkhebert <bondman2004tippy@gmail.com>
2025-01-08 13:58:28 -0600

    DEBUG webpack by switching to resolve to extensions

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,6 @@ const DIST_DIR = path.resolve(__dirname, 'dist');
 module.exports = {
 
   resolve: {
-    fallback: {
-      "fs": false,
-      "os": false,
-      "path": false,
-      "crypto": false
-    },
     extensions: [".js", ".jsx", ".json"],
   },
 
