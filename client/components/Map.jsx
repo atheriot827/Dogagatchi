395a5fae3c47416451bcaec1b3bd63e33909c781
bkhebert <bondman2004tippy@gmail.com>
2025-01-07 23:21:55 -0600

    REMOVE unused code

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -33,10 +33,47 @@ const Map = () => {
   // }, []);
 
   return (
-
-          <MapTile/>
-
+    <MapTile/>
   );
 };
 ///
 export default Map;
+
+{/* <div>
+<Stage width={800} height={600} options={{ background: 0x1099bb }}>
+  <Graphics>
+    {(Map) => {
+      const graphics = new PIXI.Graphics();
+      graphics.beginFill(0xff0000);
+      graphics.drawRect(50, 50, 100, 100);
+      graphics.endFill();
+      return graphics;
+    }}
+  </Graphics>
+  <Sprite image={bunnyUrl} x={300} y={150} />
+  <Sprite image={bunnyUrl} x={500} y={150} />
+  <Sprite image={bunnyUrl} x={400} y={200} />
+
+  <Container x={200} y={200}>
+    <Text
+      text="Hello World"
+      anchor={0.5}
+      x={220}
+      y={150}
+      filters={[blurFilter]}
+      style={
+        new TextStyle({
+          align: 'center',
+          fill: '0xffffff',
+          fontSize: 50,
+          letterSpacing: 20,
+          dropShadow: true,
+          dropShadowColor: '#E72264',
+          dropShadowDistance: 6,
+        })
+      }
+    />
+  </Container>
+</Stage>
+      <MapTile/>
+</div> */}
