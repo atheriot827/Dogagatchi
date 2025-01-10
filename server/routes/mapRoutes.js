2e23676714b32b3f02416e7f2ac937e05f1b2a3b
bkhebert <bondman2004tippy@gmail.com>
2025-01-09 19:06:33 -0600

    ADD routes to handle when dog walks onto items and weapons

--- server/routes/mapRoutes.js
+++ server/routes/mapRoutes.js
@@ -4,33 +4,57 @@ const { Dog, User } = require('../db/index');
 
 router.post('/exit', (req, res) => {
   console.log('post to the exit test');
-  // console.log(req.body);
   const { dog, user } = req.body.walkerInfo;
-  console.log(user, 'the user');
-  console.log(dog, 'the dog');
   Dog.findOneAndUpdate(
     { owner: user._id, name: dog.name },
     { $inc: { health: 10, vitality: 25, speed: 1 } },
     { new: true }
   )
     .then((walkedDog) => {
-      console.log('success!');
       console.log(walkedDog);
       res.redirect('/user');
     })
     .catch((error) => {
       console.error(error, 'error exiting map');
+      res.sendStatus(500);
     });
-  //     User.findById(userId).then(({breeds}) => {
-  //         res.status(200).send({dogsArr, breeds});
-  //     }).catch((err) => {
-  //         console.error('SERVER ERROR: failed to GET user breeds list by id', err);
-  //         res.sendStatus(500);
-  //     });
-  // }).catch((err) => {
-  //     console.error('SERVER ERROR: failed to GET dog by userId', err);
-  //     res.sendStatus(500);
-  // });
+});
+
+router.post('/weapon', (req, res) => {
+  console.log('post to the weapon test');
+  const { dog, user } = req.body.walkerInfo;
+  Dog.findOneAndUpdate(
+    { owner: user._id, name: dog.name },
+    { $inc: { attackDmg: 15 } },
+    { new: true }
+  )
+    .then((walkedDog) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.error(error, 'error grabbing weapon');
+      res.sendStatus(500);
+    });
+});
+
+router.post('/item', (req, res) => {
+  console.log('post to the item test');
+  const { dog, user } = req.body.walkerInfo;
+  console.log(user, 'the user');
+  console.log(dog, 'the dog');
+  Dog.findOneAndUpdate(
+    { owner: user._id, name: dog.name },
+    { $inc: { health: 10, vitality: 5 } },
+    { new: true }
+  )
+    .then((walkedDog) => {
+      console.log(walkedDog);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.error(error, 'error exiting map');
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
