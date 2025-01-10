960ce9079423efa9abe9f0f7a0e0b906ac1bedec
coltongraygg <coltongraygg@gmail.com>
2025-01-09 19:30:22 -0600

    FIX bug that disabled repeated toast messages

--- client/components/ToastPopUp.jsx
+++ client/components/ToastPopUp.jsx
@@ -3,30 +3,33 @@ import {useState, useEffect} from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Toast from 'react-bootstrap/Toast';
-import Button from 'react-bootstrap/Button';
+
 
 export default function ToastPopUp({message}) {
     const [showToast, setShowToast] = useState(true);
     const toggleShowToast = () => setShowToast(!showToast);
 
 
+    useEffect(() => {
+        if (message) {
+            setShowToast(true);
+        }
+    }, [message]);
+
+
     return (<>
 
 
         <Row>
-            <Col md={6} className='mb-2'>
-                <Button onClick={toggleShowToast} className='mb-2'>
-                    {message}
-                </Button>
-
-                <Toast show={showToast} onClose={toggleShowToast}>
+            <Col md={8} className='mb-2'>
+                <Toast show={showToast} onClose={toggleShowToast} animation={true} delay={4000} autohide>
                     <Toast.Header>
-                        <img src='../assets/roman.webp' className='rounded me-2'
+                        <img src='../assets/roman.webp' className='rounded me-2 w-25 h-25'
                              alt=''/>
                         <strong className='me-auto'>Roman Bellic</strong>
                         <small>Dog Trainer</small>
                     </Toast.Header>
-                    <Toast.Body>Cousin</Toast.Body>
+                    <Toast.Body>{message}</Toast.Body>
                 </Toast>
             </Col>
         </Row>
